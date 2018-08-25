ecc20765cf9933f784cfa55035feff66eb7ef9c5
Timothy Ege <retibra@gmail.com>
2018-08-25 20:03:06 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,10 +1,10 @@
 class Pirate
   attr_accessor :name, :height, :weight
 
-  def initialize(name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
+  def initialize(hash)
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
   end
 
 end
